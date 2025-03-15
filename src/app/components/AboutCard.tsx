8573d88373eeb845fbda551993cb96a6e734025d
Olushola <ememobongumoren2@gmail.com>
2025-03-15 16:27:11 +0100

    About card

--- src/app/components/AboutCard.tsx
+++ src/app/components/AboutCard.tsx
@@ -1,29 +1,20 @@
-import { Progress } from 'antd'
-import React from 'react'
+
 
 const AboutCard = () => {
 
   return (
     <>
         <div className="bg-white shadow-md border w-[60%] py-16 px-14  ">
-            <div className='divide-x-2 flex gap-28 '>
-                <div className='flex flex-col gap-3 pr-10'>
-                    <h3 className='font-bold font-commissioner text-3xl'>$89,914</h3>
-                    <p className='text-base text-darkGray'>of $100,000 backend</p>
-                </div>
-                <div className='flex flex-col gap-3  px-10'>
-                    <h3 className='font-bold font-commissioner text-3xl'>5007</h3>
-                    <p className='text-base text-darkGray'>total backers</p>
-                </div>
-                <div className='flex flex-col gap-3  px-10'>
-                    <h3 className='font-bold font-commissioner text-3xl'>56</h3>
-                    <p className='text-base text-darkGray'>days left</p>
-                </div>
-
+            <div className="flex gap-8 flex-col">
+                <span className="text-xl font-bold">About this project</span>
+                <p className="leading-loose text-darkGray font-medium">
+                The Mastercraft Bamboo Monitor Riser is a sturdy and stylish platform that elevates your screen to a more comfortable viewing height. Placing your monitor at eye level has the potential to improve your posture and make you more comfortable while at work, helping you stay focused on the task at hand.
+                </p>
+                <p className="leading-loose text-darkGray font-medium ">
+                Featuring artisan craftsmanship, the simplicity of design creates extra desk space below your computer to allow notepads, pens, and USB sticks to be stored under the stand. 
+                </p>
             </div>
-            <div className='mt-14'>
-            <Progress percent={75} showInfo={false} strokeColor={'#3cb4ac'} />
-            </div>
+           
 
         </div>
     </>
